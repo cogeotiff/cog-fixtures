d79d92c06f4c09d53e7cf4a31d2acda1d836f804
Jeff Albrecht <geospatialjeff@gmail.com>
2020-11-10 21:51:57 -0600

    update tests

--- tests/test_cog.py
+++ tests/test_cog.py
@@ -1,4 +1,5 @@
 from rasterio.crs import CRS
+from rasterio.transform import from_origin
 from rio_cogeo.cogeo import cog_info
 from rio_cogeo.profiles import JPEGProfile
 
@@ -13,6 +14,7 @@ def test_fake_image():
         count=3,
         dtype="uint8",
         crs=CRS.from_epsg(3857),
+        transform=from_origin(1470996, 6914001, 2.0, 2.0)
     )
     profile = img.handle.profile
     assert profile["width"] == 512
@@ -33,6 +35,7 @@ def test_fake_cog():
         count=3,
         dtype="uint8",
         crs=CRS.from_epsg(3857),
+        transform=from_origin(1470996, 6914001, 2.0, 2.0)
     )
     cog = FakeCog(src_img=img, dst_kwargs=JPEGProfile(),)
 
@@ -52,6 +55,7 @@ def test_with_context():
         count=3,
         dtype="int16",
         crs=CRS.from_epsg(4326),
+        transform=from_origin(0, 0, 0.01, 0.01)
     ) as img:
         ...
 
